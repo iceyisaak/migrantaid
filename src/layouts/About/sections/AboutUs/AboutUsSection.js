c0cbf730692dd4f61fe6c296d4c6d4711ea5f9e8
iceyisaak <me@iceyisaak.com>
2022-07-26 21:01:35 +0200

    Updated AboutUsSection

--- src/layouts/About/sections/AboutUs/AboutUsSection.js
+++ src/layouts/About/sections/AboutUs/AboutUsSection.js
@@ -1,14 +1,25 @@
+import Image from 'next/image';
+
 const AboutUsSection = () => {
   return (
-    <section>
-      <h1>
+    <section className='px-52 py-32'>
+      <h1 className='text-9xl'>
         About Us
       </h1>
       <article>
-        {/* <Image /> */}
+        <Image
+          src="https://via.placeholder.com/540x370.png"
+          alt='Hero Img 3'
+          layout='intrinsic'
+          width={540}
+          height={370}
+          objectFit='contain'
+          priority
+        />
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias blanditiis aspernatur sunt nam natus veniam ratione, ducimus laborum non voluptate rerum ipsa quibusdam, commodi qui suscipit voluptates perferendis sit assumenda maiores dolorum sequi saepe quo esse. Laudantium, exercitationem quidem consequuntur esse dicta dolorem. Non recusandae voluptate nisi magnam placeat repudiandae inventore accusantium quia incidunt, sit quae ullam deserunt veniam autem eaque, animi totam! Tempora, laboriosam repellendus? Repudiandae libero, natus ipsum, est illo quo id dignissimos asperiores aut ea unde similique soluta voluptates impedit eaque explicabo mollitia reiciendis suscipit repellendus quis nesciunt exercitationem. 
-          
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias blanditiis aspernatur sunt nam natus veniam ratione, ducimus laborum non voluptate rerum ipsa quibusdam, commodi qui suscipit voluptates perferendis sit assumenda maiores dolorum sequi saepe quo esse. Laudantium, exercitationem quidem consequuntur esse dicta dolorem. Non recusandae voluptate nisi magnam placeat repudiandae inventore accusantium quia incidunt, sit quae ullam deserunt veniam autem eaque, animi totam! Tempora, laboriosam repellendus? Repudiandae libero, natus ipsum, est illo quo id dignissimos asperiores aut ea unde similique soluta voluptates impedit eaque explicabo mollitia reiciendis suscipit repellendus quis nesciunt exercitationem.
+        </p>
+        <p>
           Corrupti, delectus autem, voluptates ea hic, debitis ipsam voluptatem maxime nulla amet sit! Doloremque quo reprehenderit iure, quis veritatis, nobis ab vero sapiente labore consequatur necessitatibus, rem fugiat eligendi beatae! Iure accusamus repellendus consequuntur nesciunt rerum sequi, blanditiis doloremque, fugiat at commodi deserunt dolor. Distinctio provident earum corrupti adipisci labore blanditiis nesciunt dolor praesentium veniam nobis repellendus quidem qui voluptatem enim ullam assumenda dolorem eveniet minus quo facilis laboriosam, fugit atque unde debitis! Illum debitis consequatur est distinctio dolor? Enim rem explicabo odio optio molestiae nobis autem dolores quas quidem! Expedita possimus repudiandae aliquid nihil perferendis? Illum illo aperiam cupiditate optio soluta inventore, quibusdam suscipit possimus tempore non.
         </p>
       </article>
--- src/layouts/Home/sections/Hero/HeroSection.js
+++ src/layouts/Home/sections/Hero/HeroSection.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <section className="min-h-5/6 flex px-52 py-32 ">
+    <section className="min-h-5/6 flex px-52 py-32">
       <div className="w-1/3 self-center">
         <h1 className="text-9xl mb-11">
           Peace through integration
@@ -18,7 +18,8 @@ const HeroSection = () => {
       </div>
       <div className="image-section w-2/3 flex px-32 py-12">
         <div className='w-3/6 mr-2 block'>
-          <Image src="https://via.placeholder.com/320x420.png"
+          <Image
+            src="https://via.placeholder.com/320x420.png"
             alt='Hero Img 1'
             layout='responsive'
             width={320}
@@ -29,7 +30,8 @@ const HeroSection = () => {
         </div>
         <div className='w-3/6 '>
           <div className='mt-2 mb-3 block'>
-            <Image src="https://via.placeholder.com/320x200.png"
+            <Image
+              src="https://via.placeholder.com/320x200.png"
               alt='Hero Img 2'
               layout='responsive'
               width={320}
@@ -38,7 +40,8 @@ const HeroSection = () => {
               priority
             />
           </div>
-          <Image src="https://via.placeholder.com/320x200.png"
+          <Image
+            src="https://via.placeholder.com/320x200.png"
             alt='Hero Img 3'
             layout='responsive'
             width={320}
