0fdf5df9905ea85ba866e229f4443dfcb0e1a75f
iceyisaak <me@iceyisaak.com>
2022-07-27 21:05:18 +0200

    Updated TeamSection

--- src/layouts/About/sections/Team/TeamSection.js
+++ src/layouts/About/sections/Team/TeamSection.js
@@ -1,6 +1,15 @@
+import Button from "../../../../components/Button";
+
 const TeamSection = () => {
   return (
-    <div>TeamSection</div>
+    <section className="py-72 px-52 flex flex-col items-center">
+      <h2 className="text-8xl font-heading text-center mb-52">
+        Our Team
+      </h2>
+      <div className="flex">
+      </div>
+      <Button btnText="See Our Team" />
+    </section>
   );
 };
 export default TeamSection;
